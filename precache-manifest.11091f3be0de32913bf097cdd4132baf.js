32f584fc52d42e3324c120f2d6102d8e30cc9813
T.J. Trimble <dantiston@gmail.com>
2019-02-02 20:38:35 -0800

    Updates

---
+++ precache-manifest.11091f3be0de32913bf097cdd4132baf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "74e38717a54c0e76a1df",
+    "url": "/static/css/main.40e1ed6a.chunk.css"
+  },
+  {
+    "revision": "74e38717a54c0e76a1df",
+    "url": "/static/js/main.74e38717.chunk.js"
+  },
+  {
+    "revision": "fe7ac21a2b4d9edc0c7c",
+    "url": "/static/js/1.fe7ac21a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "40ac4c90b4a187280c80765bf37243ba",
+    "url": "/static/media/RainbowGeneric_Black.40ac4c90.png"
+  },
+  {
+    "revision": "d8b9181d9b2a01fdc0224a0622183709",
+    "url": "/static/media/RainbowGeneric_Blue.d8b9181d.png"
+  },
+  {
+    "revision": "e363ecb8268616632a7eb1773c50d8fa",
+    "url": "/static/media/RainbowGeneric_Brown.e363ecb8.png"
+  },
+  {
+    "revision": "efa46d48da672a45d4a2b4fe89a442ef",
+    "url": "/static/media/RainbowGeneric_Green.efa46d48.png"
+  },
+  {
+    "revision": "f8f0fbd482db1be45d419b0313ca56b6",
+    "url": "/static/media/RainbowGeneric_Orange.f8f0fbd4.png"
+  },
+  {
+    "revision": "9b6d301295f275e9e1c03bfec76cf85a",
+    "url": "/static/media/RainbowGeneric_Purple.9b6d3012.png"
+  },
+  {
+    "revision": "4a8cab5bda5c96c7001e482132317017",
+    "url": "/static/media/RainbowGeneric_Red.4a8cab5b.png"
+  },
+  {
+    "revision": "f77440e9cfff2a3034e9995c2d626b83",
+    "url": "/static/media/RainbowGeneric_Yellow.f77440e9.png"
+  },
+  {
+    "revision": "21ace84740c30de07ae031309d985bf6",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f57388fcc8749d08f47b70e07f3fa427.js"
+  "/precache-manifest.11091f3be0de32913bf097cdd4132baf.js"
 );
 
 workbox.clientsClaim();
